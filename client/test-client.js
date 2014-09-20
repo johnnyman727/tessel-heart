242065040c5e84ec3e2ea2eeabb76c60aaee8b17
johnnyman727 <johnnyman727@gmail.com>
2014-09-19 18:45:01 -0700

    Moves the websocket to a JSON interface

--- client/test-client.js
+++ client/test-client.js
@@ -6,6 +6,41 @@ var port = 8000;
 ws.setBinaryFragmentation(1);
 
 // When we get a connection (Put your Tessel's IP Address here!)
-var connection = ws.connect('ws://192.168.128.205:' + port, function() {
-  process.stdin.pipe(connection.beginBinary());
+var connection = ws.connect('ws://172.20.10.2:' + port, function() {
+
+  connection.on('text', function(data) {
+    console.log('got some data', data);
+  });
+
+  var tesselHeart =
+  { 
+    set : {
+      heart : {
+        color : "green",
+        pulse : 50,
+      },
+      traces : {
+        1 : {
+          color : "red",
+          progress : 0.25,
+        },
+        2 : {
+          color : "white",
+          progress : 1.0,
+        }
+      }
+    },
+  };
+
+  connection.sendText(JSON.stringify(tesselHeart));
+
+  setTimeout(function() {
+    var packet = {
+      get : {
+        heart : true
+      }
+    }
+
+    connection.sendText(JSON.stringify(packet));
+  }, 1000);
 });
--- index.js
+++ index.js
@@ -3,35 +3,73 @@ var Neopixels = require('neopixels'),
     ws = require('nodejs-websocket'),
     numPixels = 60,
     neopixels = new Neopixels(numPixels),
-    prop = 0.0,
-    port = 8000;
+    port = 8000,
+    _heart = {},
+    _traces = {};
 
 
 // Create the websocket server, provide connection callback
 var server = ws.createServer(function (conn) {
   console.log("Accepted new connection...");
 
-  // If get a binary stream is opened up
-  conn.on("binary", function(stream) {
-    // When we get data
-    stream.on('data', function(data) {
-      var data = data.toString().split(' ');
-      var color = data[0];
-      var prog = parseFloat(data[1])
-      if (color) {
-        animateProgressBar(color, prog);
+  // When we get a new state
+  conn.on("text", function(data) {
+    // Gather the JSON state
+    var incoming = JSON.parse(data);
+
+    console.log('got this incoming data', incoming);
+    // 
+    if (incoming.set) {
+
+      // Animate the pulsating heart
+      animateTesselHeart(incoming.set.heart);
+
+      // Animate the progress traces 
+      animateTesselTraces(incoming.set.traces);
+    }
+
+    if (incoming.get) {
+
+      var packet = {};
+
+      if (incoming.get.heart) {
+
+        packet.heart = _heart;
       }
-      else {
-        console.log("Invalid Color Received", color);
+
+      if (incoming.get.traces) {
+        
+        packet.traces = _traces;
       }
-      
-    });
+
+      conn.sendText(JSON.stringify(packet));
+    } 
   });
 
   conn.on("close", function (code, reason) {
-      console.log("Connection closed")
+    console.log("Connection closed")
   });
+
 }).listen(port);
+
+function animateTesselHeart(heart) {
+  if (!heart) return;
+  console.log('setting ', heart);
+  _heart = heart;
+
+  // animateProgressBar(heart.color, heart.progress);
+}
+
+function animateTesselTraces(traces) {
+  if (!traces) return;
+  console.log('setting traces');
+  _traces = traces;
+
+  for (trace in traces) {
+    console.log('trace', traces[trace]);
+    // animateProgressBar(traces[trace].color, traces[trace].progress);
+  }
+}
 
 function animateProgressBar (color, progress) {
   neopixels.animate(numPixels, animations.progressBar(numPixels, color, progress));
